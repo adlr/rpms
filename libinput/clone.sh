41756df3ac717cb3af0fea30e7c536fe721e7d25
Andrew de los Reyes <adlr@rivosinc.com>
2024-07-03 06:23:21 -0700

    libinput: clone src

--- libinput/clone.sh
+++ libinput/clone.sh
@@ -2,6 +2,12 @@
 
 cd "$(dirname "$0")/.."
 
+git clone git@github.com:adlr/libinput.git libinput-src
+cd libinput-src
+git remote add upstream https://gitlab.freedesktop.org/libinput/libinput.git
+git fetch upstream
+cd ..
+
 git clone git@github.com:adlr/libinput-fedora.git libinput-rpm
 cd libinput-rpm
 git remote add upstream https://src.fedoraproject.org/rpms/libinput.git
