6484b5708ca0899109beaa50c02bc7bb5d14c48e
Andrew de los Reyes <adlr@rivosinc.com>
2024-12-07 11:25:23 -0800

    vte291: script to fetch source

--- vte291/clone.sh
+++ vte291/clone.sh
@@ -2,7 +2,14 @@
 
 cd "$(dirname "$0")/.."
 
+git clone git@github.com:adlr/vte.git vte291-src
+cd vte291-src
+git remote add upstream https://gitlab.gnome.org/GNOME/vte.git
+git fetch upstream
+cd ..
+
 git clone git@github.com:adlr/vte291-fedora.git vte291-rpm
 cd vte291-rpm
 git remote add upstream https://src.fedoraproject.org/rpms/vte291.git
 git fetch upstream
+
